d5d1ccbfb294c151432d8eb035d2905b552e56e3
Adam Křivka <krivka.adam@gmail.com>
2021-11-02 21:47:41 -0700

    got brackets matching and autoclosing working

--- src/cm6_test/views.cljs
+++ src/cm6_test/views.cljs
@@ -2,22 +2,47 @@
   (:require [reagent.core :as r]
             [applied-science.js-interop :as j]
             ["@codemirror/state" :refer [EditorState]]
-            ["@codemirror/view" :refer [EditorView keymap]]
+            ["@codemirror/view" :refer [EditorView keymap drawSelection]]
+            ["@codemirror/fold" :refer [foldGutter]]
             ["@codemirror/commands" :refer [defaultKeymap]]
+            ["@codemirror/history" :refer [history historyKeymap]]
+            ["@codemirror/highlight" :refer [defaultHighlightStyle]]
+            ["@codemirror/gutter" :refer [lineNumbers]]
+            ["@codemirror/matchbrackets" :refer [bracketMatching]]
+            ["@codemirror/closebrackets" :refer [closeBrackets]]
             ["@codemirror/lang-javascript" :refer [javascript]]))
 
 (defn editor []
   (r/with-let
-    [mount!
-     (fn [el]
-       (new EditorView
-            (j/obj :state
-                   (.create EditorState
-                            #{:extensions [(.of keymap defaultKeymap), (javascript)]})
-                   :parent el)))]
+    [extensions #js[(.of keymap
+                         (concat defaultKeymap historyKeymap))
+                    (.. EditorView -lineWrapping)
+                    (.. EditorState -allowMultipleSelections (of true))
+                    (drawSelection)
+                    (bracketMatching)
+                    (closeBrackets)
+                    (history)
+                    defaultHighlightStyle
+                    (lineNumbers)
+                    (foldGutter)
+                    (javascript)]
+     mount! (fn [el]
+              (when el
+                (new EditorView
+                     (j/obj :state (.create EditorState #js{:extensions extensions})
+                            :parent el))))]
     [:div
      {:ref mount!}]))
 
-(defn app []
-  [:div.w-96.h-48.m-auto.mt-24.bg-red-50
+(defn lang-chooser []
+  [:div.absolute])
+
+(defn full-editor []
+  [:<>
+   [lang-chooser]
    [editor]])
+
+(defn app []
+  [:div.w-96.h-48.m-auto.mt-24
+   [:div.rounded-md.overflow-auto.relative.border.shadow-lg.bg-white
+    [full-editor]]])
